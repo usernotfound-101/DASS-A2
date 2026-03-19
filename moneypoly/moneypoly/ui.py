efe4a7709efe67d54b837ccbebc75f97cbfb8210
usernotfound-101 <ayush.moharana2006@gmail.com>
2026-03-19 14:17:09 +0530

    Iteration 8: Added docstring and added ValueError to except in ui.py

--- moneypoly/moneypoly/ui.py
+++ moneypoly/moneypoly/ui.py
@@ -1,3 +1,5 @@
+"""Console UI helpers for MoneyPoly."""
+
 def print_banner(title):
     """Print a decorated section header."""
     width = 52
@@ -66,7 +68,7 @@ def safe_int_input(prompt, default=0):
     """
     try:
         return int(input(prompt))
-    except:
+    except ValueError:
         return default
 
 
