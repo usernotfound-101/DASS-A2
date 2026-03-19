2f80c244f332677c150b65fcbf8bcd8dce7ce8f4
usernotfound-101 <ayush.moharana2006@gmail.com>
2026-03-19 15:26:20 +0530

    Iteration 16: Added docstring and reformatted _create_properties. Added a for loop to construct and return a list of properties in board.py

--- moneypoly/moneypoly/board.py
+++ moneypoly/moneypoly/board.py
@@ -1,3 +1,5 @@
+"""Board model and tile lookup utilities for MoneyPoly."""
+
 from .property import Property, PropertyGroup
 from .config import (
     JAIL_POSITION,
@@ -52,30 +54,36 @@ class Board:
     def _create_properties(self):
         """Instantiate every purchasable property and return as a list."""
         g = self.groups
-        return [
-            Property("Mediterranean Avenue",   1,  60,  2,  g["brown"]),
-            Property("Baltic Avenue",          3,  60,  4,  g["brown"]),
-            Property("Oriental Avenue",        6,  100, 6,  g["light_blue"]),
-            Property("Vermont Avenue",         8,  100, 6,  g["light_blue"]),
-            Property("Connecticut Avenue",     9,  120, 8,  g["light_blue"]),
-            Property("St. Charles Place",      11, 140, 10, g["pink"]),
-            Property("States Avenue",          13, 140, 10, g["pink"]),
-            Property("Virginia Avenue",        14, 160, 12, g["pink"]),
-            Property("St. James Place",        16, 180, 14, g["orange"]),
-            Property("Tennessee Avenue",       18, 180, 14, g["orange"]),
-            Property("New York Avenue",        19, 200, 16, g["orange"]),
-            Property("Kentucky Avenue",        21, 220, 18, g["red"]),
-            Property("Indiana Avenue",         23, 220, 18, g["red"]),
-            Property("Illinois Avenue",        24, 240, 20, g["red"]),
-            Property("Atlantic Avenue",        26, 260, 22, g["yellow"]),
-            Property("Ventnor Avenue",         27, 260, 22, g["yellow"]),
-            Property("Marvin Gardens",         29, 280, 24, g["yellow"]),
-            Property("Pacific Avenue",         31, 300, 26, g["green"]),
-            Property("North Carolina Avenue",  32, 300, 26, g["green"]),
-            Property("Pennsylvania Avenue",    34, 320, 28, g["green"]),
-            Property("Park Place",             37, 350, 35, g["dark_blue"]),
-            Property("Boardwalk",              39, 400, 50, g["dark_blue"]),
+        specs = [
+            ("Mediterranean Avenue", 1, 60, 2, "brown"),
+            ("Baltic Avenue", 3, 60, 4, "brown"),
+            ("Oriental Avenue", 6, 100, 6, "light_blue"),
+            ("Vermont Avenue", 8, 100, 6, "light_blue"),
+            ("Connecticut Avenue", 9, 120, 8, "light_blue"),
+            ("St. Charles Place", 11, 140, 10, "pink"),
+            ("States Avenue", 13, 140, 10, "pink"),
+            ("Virginia Avenue", 14, 160, 12, "pink"),
+            ("St. James Place", 16, 180, 14, "orange"),
+            ("Tennessee Avenue", 18, 180, 14, "orange"),
+            ("New York Avenue", 19, 200, 16, "orange"),
+            ("Kentucky Avenue", 21, 220, 18, "red"),
+            ("Indiana Avenue", 23, 220, 18, "red"),
+            ("Illinois Avenue", 24, 240, 20, "red"),
+            ("Atlantic Avenue", 26, 260, 22, "yellow"),
+            ("Ventnor Avenue", 27, 260, 22, "yellow"),
+            ("Marvin Gardens", 29, 280, 24, "yellow"),
+            ("Pacific Avenue", 31, 300, 26, "green"),
+            ("North Carolina Avenue", 32, 300, 26, "green"),
+            ("Pennsylvania Avenue", 34, 320, 28, "green"),
+            ("Park Place", 37, 350, 35, "dark_blue"),
+            ("Boardwalk", 39, 400, 50, "dark_blue"),
         ]
+        properties = []
+        for name, position, price, base_rent, group_key in specs:
+            prop = Property(name, position, price, base_rent)
+            g[group_key].add_property(prop)
+            properties.append(prop)
+        return properties
 
     def get_property_at(self, position):
         """Return the Property at `position`, or None if there is none."""
