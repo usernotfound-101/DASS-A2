a8ab7ae765c64423058ea17e22c9f3ed73bd3ffa
usernotfound-101 <ayush.moharana2006@gmail.com>
2026-03-19 14:18:32 +0530

    Iteration 9: Added docstring, removed unused .config import and aded doubles_streak value set in dice.py

--- moneypoly/moneypoly/dice.py
+++ moneypoly/moneypoly/dice.py
@@ -1,5 +1,6 @@
+"""Dice mechanics for MoneyPoly."""
+
 import random
-from .config import BOARD_SIZE
 
 class Dice:
     """Simulates a pair of six-sided dice with consecutive-doubles tracking."""
@@ -7,6 +8,7 @@ class Dice:
     def __init__(self):
         self.die1 = 0
         self.die2 = 0
+        self.doubles_streak = 0
         self.reset()
 
     def reset(self):
